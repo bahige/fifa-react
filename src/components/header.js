4d0367a5141204e43d8c8afb3669fded6bdaa001
Bahige <bahigesaab8@gmail.com>
2020-06-10 18:18:06 +0300

    Header created.

---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import WorldCupLogo from "../assets/wc_logo.png";
+import Facebook from "../assets/facebook.png";
+import Twitter from "../assets/twitter.png";
+import Instagram from "../assets/instagram.png";
+import Menu from "../assets/three_menu.png";
+import headerstyle from "../components/header.module.css";
+
+const Header = () => {
+  return (
+    <div className={headerstyle.header}>
+      <img
+        className={headerstyle.logo}
+        src={WorldCupLogo}
+        alt="World Cup Logo"
+      ></img>
+      <div className={headerstyle.social_media_container}>
+        <img
+          className={headerstyle.social_media}
+          src={Facebook}
+          alt="facebook_logo"
+        ></img>
+        <img
+          className={headerstyle.social_media}
+          src={Twitter}
+          alt="twitter_logo"
+        ></img>
+        <img
+          className={headerstyle.social_media}
+          src={Instagram}
+          alt="instagram_log"
+        ></img>
+        <img className={headerstyle.social_media} src={Menu} alt="menu"></img>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
